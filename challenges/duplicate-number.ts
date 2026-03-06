d3ad55c974ce373ccf4379b5e5829345327d8ed7
Lawrenzo Lue <lawrenzo.lue@macaulay.cuny.edu>
2026-03-06 09:21:33 -0500

    working on optimizing duplicate number

--- challenges/duplicate-number.ts
+++ challenges/duplicate-number.ts
@@ -4,11 +4,31 @@
  * If no duplicates are found, return 0.
  * ex: [1,5,4,3,6,2,4,7] should return 4
  */
-
+//input = an array of n + 1 numbers with distinct numbers except 1,
+//output - either 0 if no duplicates, or # if duplicate
+//one loop to go through the array and another to check whether there's a copy of that number
 export const duplicateNumber = (array: number[]): number => {
+  // for (let i = 0; i < array.length; i++) {
+  //   for (let j = i + 1; j < array.length; j++) {
+  //     if (array[i] === array[j]) {
+  //       return array[i];
+  //     } else {
+  //       console.log(array[i], ' arrayj: ', array[j]);
+  //       continue;
+  //     }
+  //   }
+  // }
+  for (let i = 0; i < array.length; i++) {
+    
+  }
+  const set1 = new Set(array);
+  // const set2 = new Set(array);
+
+  console.log(set1);
   return 0;
 };
 
+console.log(duplicateNumber([1, 5, 3, 3, 0, 4]));
 /*
  * Extension:
  * You have an unsorted array of n + 1 positive integers, ranging from k to k +
@@ -19,5 +39,8 @@ export const duplicateNumber = (array: number[]): number => {
  */
 
 export const duplicateNumberAdvanced = (array: number[]): number => {
+  //o(n)
+
+  //o(1)
   return 0;
 };
--- challenges/pow.ts
+++ challenges/pow.ts
@@ -6,19 +6,44 @@
  * pow(2,4) => 2^4 = 16
  * Rational: 2 * 2 * 2 * 2 = 16
  *
+ * input = x, base value to be raised to the y(positive) exponent
+ * output - the result of x^y
+ *
+ * take x, and multiply it by itself y times
+ * Because 0 is neither positive nor negative, we don't have to factor in 0 as the y value
+ *
+ * if y is 1, then return result, but if not, then loop result multiplied by x y times
  * Extension: Use recursion.
  */
 
 function pow(base: number, power: number): number {
-  return 0; // Placeholder return value for type safety - replace for your solution
-}
+  let result = base;
+  if (power === 0) return 1;
+  if (power === 1) return base;
+  //the issue was that I put 0 instead of 1 here.
+  for (let i = 1; i < power; i++) {
+    // console.log('base ', base);
+    // console.log('result ', result);
+    result *= base;
+    ``;
+  }
 
+  return result; // Placeholder return value for type safety - replace for your solution
+}
+// console.log(pow(5, 2));
+// pow(3, 4);
+// pow(2, 5);
+// pow(2, 4);
 /**
  * Extension: Use recursion.
  */
 
 function powRecurse(base: number, power: number): number {
-  return 0; // Placeholder return value for type safety - replace for your solution
+  let result = base;
+  if (power === 0) return 1;
+  if (power === 1) return base;
+
+  return result * powRecurse(base, power - 1); // Placeholder return value for type safety - replace for your solution
 }
 
 export { pow, powRecurse };
